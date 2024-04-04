90193eab15e51d509724bfeb5e8d134420717675
RikuyaChiba <landlixer@gmail.com>
2024-04-04 18:00:19 +0900

    fix

--- src/server.mjs
+++ src/server.mjs
@@ -1,15 +1,24 @@
 // server.mjs
 import { createServer } from 'node:http';
 
+const PORT = 3000;
+
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!\n');
 });
 
+server.on("upgrade", (req, socket, head) => {
+  socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
+    'Upgrade: WebSocket\r\n' +
+    'Connection: Upgrade\r\n' +
+    '\r\n');
+});
+
+
 // starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(PORT, '0.0.0.0', () => {
+  console.log("Listening to port" + PORT);
 });
 
 // run with `node server.mjs`
-
